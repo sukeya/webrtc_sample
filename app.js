e03988862e5f38f42fa08d21017e6abe1447da06
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2022-07-11 12:31:05 +0900

    add `Open camera` button.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-async function getMedia(constraints) {
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    document.querySelector('video').srcObject = stream;
-}
-  
+`use strict`;
+
 var constraints = {
     audio: true,
     video: true
 };
 
-getMedia(constraints);
+async function getLocalMedia(e) {
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        document.querySelector('#localVideo').srcObject = stream;
+        e.target.disabled = true;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+document.querySelector('#openLocalVideo').addEventListener('click', e => getLocalMedia(e));
