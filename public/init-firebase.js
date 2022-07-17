82ddc6b0c5025cd480b96ef71c912cbb5ad3b233
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2022-07-17 14:06:38 +0900

    add a script initializing firebase.

---
+++ public/init-firebase.js
@@ -0,0 +1,21 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDfeWiwQTubuDDpaeGZZYR2DTQESZfuKyE",
+  authDomain: "fir-rtc-fa7f8.firebaseapp.com",
+  databaseURL: "https://fir-rtc-fa7f8-default-rtdb.firebaseio.com",
+  projectId: "fir-rtc-fa7f8",
+  storageBucket: "fir-rtc-fa7f8.appspot.com",
+  messagingSenderId: "46016288441",
+  appId: "1:46016288441:web:5baf0d35111754e5e0ad68",
+  measurementId: "G-HTQKZ0RF2X"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
