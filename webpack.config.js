31768f8ef83577039c277059e3ff1acfc385dcb9
Yuya Asano <64895419+sukeya@users.noreply.github.com>
2022-07-31 20:03:41 +0900

    add webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/app.scss', './src/app.js', './src/init-firebase.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: `development`,
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+
+              // See https://github.com/webpack-contrib/sass-loader/issues/804
+              webpackImporter: false,
+              sassOptions: {
+                includePaths: ['./node_modules']
+              },
+            },
+          },
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        }
+      }
+    ]
+  },
+};
