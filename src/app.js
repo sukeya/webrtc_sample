9d5ff290e1d5532c25e7c6424eabd193c087fe77
sukeya <64895419+sukeya@users.noreply.github.com>
2022-08-16 22:17:53 +0900

    fix the urls of STUN servers.

--- src/app.js
+++ src/app.js
@@ -20,8 +20,7 @@ let constraints = {
 const configuration = {
   iceServers: [{
     urls: [
-      'stun:stun1.l.google.com:19302',
-      'stun:stun2.l.google.com:19302'
+      'stun:stun.l.google.com:19302',
     ]
   }],
   iceTransportPolicy: `all`,
